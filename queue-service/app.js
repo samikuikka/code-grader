fe8e50ceebd6a99d235016056786d1c71cc237bb
Sami Kuikka <sami.kuikka@aalto.fi>
2022-11-18 11:04:56 +0200

    websocket working

--- queue-service/app.js
+++ queue-service/app.js
@@ -1,10 +1,15 @@
 import { delay, serve } from "./deps.js";
+import { grade } from "./grade.js";
 
-const sockets = new Set();
+const sockets = new Map();
 
 const createWebSocketConnection = (request) => {
     console.log("Creating WS connection");
     const { socket, response } = Deno.upgradeWebSocket(request);
+
+    const url = new URL(request.url);
+    const params = url.searchParams;
+    const id = params.get('id');
   
     socket.onopen = () => socket.send("Connection created");
     socket.onmessage = (e) => console.log(`Received a message: ${e.data}`);
@@ -15,26 +20,58 @@ const createWebSocketConnection = (request) => {
     };
     socket.onerror = (e) => console.error("WS error:", e);
   
-    sockets.add(socket);
+    sockets.set(id, socket);
   
     return response;
 };
 
+let queue = [];
+let run = 0;
+
+const runQueue = async () => {
+    if(!queue.length) {
+        return;
+    }
+    if(!run) {
+        return;
+    }
+    run = 1;
+    
+    let item = queue.pop();
+    console.log('Processing ', item);
+    await delay(5000);
+
+    //MOCK GRADE
+    let res = {
+        result: null
+    }
+    Math.random() * 3 < 2 ? res.result = true : res.result = false;
+
+    //Send grade
+    sockets.get(item.user).send(JSON.stringify(res));
+    
+    if(queue.length > 0) {
+        return runQueue();
+    }
+
+    run = 0;
+    return;
+}
+
 const handleRequest = async (request) => {
-    const pathname = new URL(request.url).pathname;
-    console.log(pathname);
+    const url = new URL(request.url);
+    const pathname = url.pathname;
+    //console.log(pathname);
     if (pathname == "/" && request.method == 'POST') {
         const data = await request.json();
-        console.log('Data in the queue service', data)
+        console.log('Adding to queue', data)
+        queue.push(data);
 
-        //MOCK GRADE
-        let res = {
-            result: null
+        if(run == 0) {
+            console.log("Started the service")
+            run = 1;
+            runQueue();
         }
-        Math.random() * 3 < 2 ? res.result = true : res.result = false;
-        
-        //Send grade
-        sockets.forEach(socket => socket.send(JSON.stringify(res)));
     } else if (pathname === "/connect") {
       return createWebSocketConnection(request);
     }
